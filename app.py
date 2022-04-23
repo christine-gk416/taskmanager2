130e18a1b7c71d2de09787a877780df3f02b3132
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-04-23 11:31:00 +0000

    Add basic registration functoin

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
 import os
 from bson.objectid import ObjectId
 from flask import (
-    Flask, flash, render_template, request, session, url_for)
+    Flask, flash, render_template, redirect, request, session, url_for)
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_pymongo import PyMongo
-if os.path.exists('env.py'):
+if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.secret_key = os.environ.get('SECRET_KEY')
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 
-@app.route('/')
-@app.route('/get_tasks')
+@app.route("/")
+@app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template('tasks.html', tasks=tasks)
+    return render_template("tasks.html", tasks=tasks)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration succussful!")
+    return render_template("register.html")
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
